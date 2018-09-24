5a49edac8b4227c4d82eea67874d21e03bb075c9
Chett Tiller <chett.tiller@gmail.com>
2018-09-24 15:17:15 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -86,8 +86,7 @@ def adding_matz
 		 #   :known_for => "Ruby",
 		 #    :languages => ["LISP", "C"]
 		 # }
-		 binding.pry
-
+		 programmer_hash
 end
 
 def changing_alan
@@ -109,8 +108,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
 		 programmer_hash[:alan_kay][:known_for] = alans_new_info
+		 programmer_hash
 
-		 binding.pry
 end
 
 def adding_to_dennis
